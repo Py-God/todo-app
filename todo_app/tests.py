4d5a40eef729777c789130979bbb4f048bf3618f
Boluwatife Promise <boluwatifelekeoduoye@gmail.com>
2022-10-10 23:41:23 +1300

    Update tests.py

--- todo_app/tests.py
+++ todo_app/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Task
+from django.utils import timezone
 
-# Create your tests here.
+
+class TaskListViewTests(TestCase):
+    def test_homepage_at_correct_url_and_uses_correct_template(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "home.html")
+
+    def test_homepage_with_correct_url_name(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "home.html")
+
+
+class TaskCreateViewTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.task = Task.objects.create(
+            title="Exercise", is_completed=False, time_due=timezone.now()
+        )
+
+    def test_post_model(self):
+        self.assertEqual(self.task.title, "Exercise")
+        self.assertEqual(str(self.task), "Exercise")
+        self.assertEqual(self.task.get_absolute_url(), "/1/")
